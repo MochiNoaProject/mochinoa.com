b3753c18b925b10f17ec35be9ccd0f18e38f2c94
hrdtbs <harada.tsubasa.0422@gmail.com>
2023-02-15 13:39:54 +0900

    refactor(pages/marry): anti-cheating

--- app/marry/MarryOff.tsx
+++ app/marry/MarryOff.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from "react";
 
 export const MarryOff = ({ className }: { className?: string }) => {
-  const [score, setScore] = useState(0);
+  const [score, setScore] = useState<number>();
   const fired = useRef(false);
   useEffect(() => {
     if (fired.current) {
@@ -27,16 +27,18 @@ export const MarryOff = ({ className }: { className?: string }) => {
   }, []);
   return (
     <div className={className}>
-      <p>あなたが結婚に同意するまで、{score}秒かかりました。</p>
-      <p>
-        {score > 0
-          ? score > 3
-            ? "なんですぐに答えられないのかな。"
-            : score > 1
-            ? "もう少し頑張りましょう。"
-            : "まあ合格やな。"
-          : "..."}
-      </p>
+      {score !== undefined ? (
+        <div>
+          <p>あなたが結婚に同意するまで、{score}秒かかりました。</p>
+          <p>
+            {score > 3
+              ? "なんですぐに答えられないのかな。"
+              : score > 1
+              ? "もう少し頑張りましょう。"
+              : "まあ合格やな。"}
+          </p>
+        </div>
+      ) : null}
     </div>
   );
 };
