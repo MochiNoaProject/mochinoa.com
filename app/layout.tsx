d9dba056c86a42df42861af3dff750556e21ef68
hrdtbs <harada.tsubasa.0422@gmail.com>
2023-04-01 18:52:38 +0900

    chore: test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,6 @@
 import "./index.css";
 import "./ress.css";
-import { Metadata } from "next";
 import GoogleAnalytics from "./GoogleAnalytics";
-
-const title = "望月のあ公式サイト";
-const description = `歌とお絵描きが好き!全て自作のVTuber望月のあの公式サイトです。活動実績や各種SNSのリンクなどを掲載しています。今すぐアクセスして魅力的な配信や活躍をチェックしてください!`;
-
-export const metadata = {
-  title: {
-    default: title,
-    template: `%s | ${title}`,
-  },
-  description,
-  icons: {
-    icon: "/favicon.png",
-  },
-  viewport: "width=device-width, initial-scale=1.0",
-  twitter: {
-    site: "@_noach",
-    creator: "@_noach",
-    card: "summary_large_image",
-    images: "https://www.mochinoa.com/img/ogp.jpg",
-  },
-  openGraph: {
-    type: "website",
-    title,
-    siteName: title,
-    url: "https://www.mochinoa.com",
-    images: "https://www.mochinoa.com/img/ogp.jpg",
-    description,
-  },
-} satisfies Metadata;
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import clsx from "clsx";
 import styles from "./page.module.css";
+
+const title = "望月のあ公式サイト";
+const description = `歌とお絵描きが好き!全て自作のVTuber望月のあの公式サイトです。活動実績や各種SNSのリンクなどを掲載しています。今すぐアクセスして魅力的な配信や活躍をチェックしてください!`;
+
+export const metadata = {
+  title: {
+    default: title,
+    template: `%s | ${title}`,
+  },
+  description,
+  icons: {
+    icon: "/favicon.png",
+  },
+  viewport: "width=device-width, initial-scale=1.0",
+  twitter: {
+    site: "@_noach",
+    creator: "@_noach",
+    card: "summary_large_image",
+    images: "https://www.mochinoa.com/img/ogp.jpg",
+  },
+  openGraph: {
+    type: "website",
+    title,
+    siteName: title,
+    url: "https://www.mochinoa.com",
+    images: "https://www.mochinoa.com/img/ogp.jpg",
+    description,
+  },
+} satisfies Metadata;
 
 export default function Page() {
   return (
