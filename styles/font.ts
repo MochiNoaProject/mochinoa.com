c21f946ff62a3be8d0ca2841990137891a2a4f7b
hrdtbs <harada.tsubasa.0422@gmail.com>
2023-06-25 21:29:45 +0900

    chore: use panda css features instead of index.css

---
+++ styles/font.ts
@@ -0,0 +1,8 @@
+import { Zen_Maru_Gothic } from "next/font/google";
+
+export const ZenMaruGothic = Zen_Maru_Gothic({
+  weight: "400",
+  style: ["normal"],
+  display: "swap",
+  subsets: [],
+});
