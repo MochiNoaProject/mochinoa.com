0d7912e5181a5a81df1bf79532231e7b32d3700f
hrdtbs <harada.tsubasa.0422@gmail.com>
2024-09-07 15:58:02 +0900

    feat: update hero section

--- app/_sections/HeroSection.tsx
+++ app/_sections/HeroSection.tsx
@@ -6,7 +6,7 @@ import styles from "./HeroSection.module.css";
 export default function HeroSection() {
   return (
     <section className={styles.root}>
-      <div>
+      <div className={styles.Background__root}>
         <div className={styles.Background__sign}>
           <Image
             src="/img/sign.png"
@@ -28,47 +28,53 @@ export default function HeroSection() {
       <div className={styles.Contents}>
         <div className={styles.Card}>
           <h2 className={styles.Title}>About 望月のあ</h2>
-          <ul className={styles.SnsList}>
-            {[
-              {
-                name: "YouTube",
-                url: "https://www.youtube.com/@MochizukiNoa",
-                icon: "/img/icon_youtube.png",
-              },
-              {
-                name: "Twitter",
-                url: "https://twitter.com/_noach",
-                icon: "/img/icon_twitter.png",
-              },
-              {
-                name: "BOOTH",
-                url: "https://mochinoa.booth.pm/",
-                icon: "/img/icon_booth.png",
-              },
-            ].map((item) => {
-              return (
-                <li key={item.name}>
-                  <a href={item.url} target="_blank" rel="noopener noreferrer">
-                    {item.icon ? (
-                      <Image
-                        src={item.icon}
-                        width={20}
-                        height={20}
-                        alt={item.name}
-                      />
-                    ) : null}
-                    {item.name}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-          <p>
-            歌とお絵描きが好きなVTuber!得意のお絵描きとLive2Dのちからで自分自身を錬成し、全て自作で活動中!
-          </p>
-          <p>
-            これまでに受けたお仕事を実績としてまとめているので、望月のあに依頼したい案件などありましたらご相談ください!食べ物のPR系や歌唱依頼、リアルイベント、その他わくわくするお仕事をたくさんできると嬉しいです！
-          </p>
+          <div className={styles.Card__content}>
+            <ul className={styles.SnsList}>
+              {[
+                {
+                  name: "YouTube",
+                  url: "https://www.youtube.com/@MochizukiNoa",
+                  icon: "/img/icon_youtube.png",
+                },
+                {
+                  name: "Twitter",
+                  url: "https://twitter.com/_noach",
+                  icon: "/img/icon_twitter.png",
+                },
+                {
+                  name: "BOOTH",
+                  url: "https://mochinoa.booth.pm/",
+                  icon: "/img/icon_booth.png",
+                },
+              ].map((item) => {
+                return (
+                  <li key={item.name}>
+                    <a
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {item.icon ? (
+                        <Image
+                          src={item.icon}
+                          width={20}
+                          height={20}
+                          alt={item.name}
+                        />
+                      ) : null}
+                      {item.name}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+            <p>
+              歌とお絵描きが好きなVTuber!得意のお絵描きとLive2Dのちからで自分自身を錬成し、全て自作で活動中!
+            </p>
+            <p>
+              これまでに受けたお仕事を実績としてまとめているので、望月のあに依頼したい案件などありましたらご相談ください!食べ物のPR系や歌唱依頼、リアルイベント、その他わくわくするお仕事をたくさんできると嬉しいです！
+            </p>
+          </div>
 
           <div>
             <Link
