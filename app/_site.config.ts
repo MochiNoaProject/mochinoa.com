a022298cbbee2794bad1d454efece23969b00414
hrdtbs <harada.tsubasa.0422@gmail.com>
2025-08-16 18:47:33 +0900

    feat: implement new AppHeader

---
+++ app/_site.config.ts
@@ -0,0 +1,10 @@
+
+export const siteConfig = {
+    youtube: {
+        url: "https://www.youtube.com/@MochizukiNoa"
+    },
+    twitter: {
+        url: "https://x.com/_noach",
+    }
+}
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,43 @@
 import Link from "next/link";
-import ActivitiesSection from "./_sections/ActivitiesSection";
-import ContactSection from "./_sections/ContactSection";
-import HeroSection from "./_sections/HeroSection";
-import HobbiesSection from "./_sections/HobbiesSection";
 import styles from "./page.module.css";
+import Image from "next/image";
+import nikukyu from "./_assets/nikukyu.png"
+import youtubeIcon from "./_assets/youtube-icon.png";
+import twitterIcon from "./_assets/twitter-icon.png";
+import { siteConfig } from "./_site.config";
 
 export default function Page() {
 	return (
-		<main>
-			<nav className={styles.AppMenu}>
-				<ul className={styles.AppMenu__list}>
-					{[
-						{
-							href: "#top",
-							label: "トップ",
-						},
-						{
-							href: "#activity",
-							label: "実績",
-						},
-						{
-							href: "#contact",
-							label: "ご依頼",
-						},
-						{
-							href: "#app",
-							label: "アプリ",
-						},
-					].map((item) => {
-						return (
-							<li key={item.label}>
-								<a href={item.href}>{item.label}</a>
-							</li>
-						);
-					})}
-				</ul>
-			</nav>
-			<HeroSection />
-			<ActivitiesSection />
-			<ContactSection />
-			<HobbiesSection />
-			<footer className={styles.Footer}>
-				<div className={styles.Footer__links}>
-					<Link href="/guidelines/streaming">配信ガイドライン</Link>
-					<Link href="/guidelines/fanart">
-						ファンアート・著作物ガイドライン
+		<div>
+			<header className={styles.AppHeader}>
+				<h1>
+					<Link href="/" className={styles.Title}>
+						<Image src={nikukyu} alt="肉球のマーク" width={16} height={14} />
+						Mochinoa's Official Site
 					</Link>
-				</div>
-				<small className={styles.Footer__copyright}>
-					&copy; 2023 もちもちクリエイト
-				</small>
-			</footer>
-		</main>
+				</h1>
+				<nav className={styles.Menu}>
+					<ul>
+						<li>
+							<Link href="#about">About</Link>
+						</li>
+						<li>
+							<Link href="#works">Works</Link>
+						</li>
+						<li>
+							<Link href="#contact">Contact</Link>
+						</li>
+					</ul>
+					<div className={styles.Socials}>
+						<Link className={styles.LinkButton}  href={siteConfig.twitter.url} target="_blank" rel="noopener noreferrer">
+							<Image src={twitterIcon} alt="Twitter" width={twitterIcon.width} height={twitterIcon.height} />
+						</Link>
+						<Link className={styles.LinkButton} href={siteConfig.youtube.url} target="_blank" rel="noopener noreferrer">
+							<Image src={youtubeIcon} alt="YouTube" width={youtubeIcon.width} height={youtubeIcon.height} />
+						</Link>
+					</div>
+				</nav>
+			</header>
+		</div>
 	);
 }
